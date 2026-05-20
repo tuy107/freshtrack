964f0eb6c2745bc1a8c56cc7da9754cbb5d64e33
toortiz43 <toortiz43@gmail.com>
2026-05-19 23:23:37 -0600

    PWA: service worker mejorado, manifest corregido, boton instalar

--- sw.js
+++ sw.js
@@ -1,14 +1,12 @@
-const CACHE = 'freshtrack-v1';
-const ARCHIVOS = [
-  './',
-  './index.html',
-  './icon-192.png',
-  './icon-512.png',
-  './manifest.json',
-];
+const CACHE = 'freshtrack-v2';
 
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ARCHIVOS)));
+  e.waitUntil(
+    caches.open(CACHE).then(c =>
+      c.addAll(['./index.html', './manifest.json', './icon-192.png', './icon-512.png'])
+       .catch(() => c.add('./index.html'))
+    )
+  );
   self.skipWaiting();
 });
 
@@ -22,7 +20,14 @@ self.addEventListener('activate', e => {
 });
 
 self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
   e.respondWith(
-    caches.match(e.request).then(cached => cached || fetch(e.request).catch(() => caches.match('./index.html')))
+    fetch(e.request)
+      .then(res => {
+        const clone = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, clone));
+        return res;
+      })
+      .catch(() => caches.match(e.request).then(cached => cached || caches.match('./index.html')))
   );
 });
